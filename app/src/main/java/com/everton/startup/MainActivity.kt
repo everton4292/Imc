7f97279f7fda49c6dc82e43df075362192370ca6
everton4292 <ecoresende@hotmail.com>
2020-04-08 13:56:25 -0300

    added score overral category and empty string crash prevent

--- app/src/main/java/com/everton/startup/MainActivity.kt
+++ app/src/main/java/com/everton/startup/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.everton.startup
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.text.DecimalFormat
@@ -12,14 +14,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
         button.setOnClickListener {
+            var altura = altura.text.toString().replace(',', '.')
+            var peso = peso.text.toString().replace(',', '.')
 
 
-            var altura = DecimalFormat.getInstance().parse(altura.text.toString().replace(",", "."))
-            var peso = DecimalFormat.getInstance().parse(peso.text.toString().replace(",", "."))
+            if (peso.isEmpty() || altura.isEmpty()) {
+                Toast.makeText(this@MainActivity, "Peso ou Altura em branco", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+
             var imc = peso.toFloat() / (altura.toFloat() * altura.toFloat())
-
             resultado.text = imc.toString()
+
+
+            if (imc < 18.5) {
+                resultado2.text = "Abaixo do peso"
+            } else if (imc in 18.5..24.9) {
+                resultado2.text = "Peso normal"
+            } else if (imc in 25.0..29.9) {
+                resultado2.text = "Sobrepeso"
+            } else if (imc in 30.0..34.9) {
+                resultado2.text = "Obesidade grau I"
+            } else if (imc in 35.0..39.9) {
+                resultado2.text = "Obesidade grau II"
+            } else if (imc > 40.0) {
+                resultado2.text = "Obesidade grau III"
+            }
             // É calculado dividindo o peso (em kg) pela altura ao quadrado (em metros).
 
         }
